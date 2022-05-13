4f9d5b34161e8da819e394dbd0b906dbb8517b8b
Diebarix <diegoabr0704@gmail.com>
2022-05-12 21:27:27 -0500

    change the password

--- src/keys.js
+++ src/keys.js
@@ -5,7 +5,7 @@ module.exports = {
     database: {
         //direccion de la base de datos
         //URI: 'mongodb://localhost/proyecto_red_social'
-        URI: 'mongodb+srv://Diebarix:Taq.ueras07*422@cluster0.1mx0z.mongodb.net/red-social-nodejs-mongodb?retryWrites=true&w=majority'
+        URI: 'mongodb+srv://Diebarix:Taqueras07422@cluster0.1mx0z.mongodb.net/red-social-nodejs-mongodb?retryWrites=true&w=majority'
 
         //se podria utilizar mongoDB ATLAS, pero en este caso lo usare de manera local
     }
